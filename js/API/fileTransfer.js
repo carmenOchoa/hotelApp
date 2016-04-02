fdec04a856dcc881c00131e79d8d6ac53c0b9952
carmenOchoa <carmen_ochoa@laitaliana.com.mx>
2016-04-02 10:46:18 -0600

    f

--- js/API/fileTransfer.js
+++ js/API/fileTransfer.js
@@ -2,9 +2,6 @@
 var ft = {
     obj: new FileTransfer(),
     win: function (r) {
-        /*console.log("Code = " + r.responseCode);
-        console.log("Response = " + r.response);
-        console.log("Sent = " + r.bytesSent);*/
         if(r.response == 1){
             window.localStorage.setItem("user",$('#regNom').val());
             window.location.href = '#home';
@@ -16,7 +13,7 @@ var ft = {
     transfer: function(fileURL){
         var options = new FileUploadOptions();
         options.fileKey = "foto";
-        options.fileName = " carmen";
+        options.fileName = "carmen";
         options.mimeType = "image/jpeg";
         
         ft.obj.upload(fileURL, "http://carlos.igitsoft.com/apps/test.php", ft.win, ft.fail, options);
