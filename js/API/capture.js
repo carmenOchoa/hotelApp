be62212704549a25c2bc35bd8c9731096b915fbd
carmenOchoa <carmen_ochoa@laitaliana.com.mx>
2016-04-02 11:58:11 -0600

    a

--- js/API/capture.js
+++ js/API/capture.js
@@ -2,9 +2,8 @@ var capture = {
     success: function(mf){
         var i, path, len;
         for (i = 0, len = mf.length; i < len; i += 1) {
-            path = mf[i].fullPath;            
+            path = mf[i].fullPath;
         }
-        
         $('#regFoto').attr('data-foto',path);
         $('#regFoto').html('<img src="'+path+'" style="width:100%;">');
     },
@@ -12,7 +11,6 @@ var capture = {
         navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error','Perfecto!');
     },
     takePhoto: function(){
-        alert(1);
         navigator.device.capture.captureImage(capture.success, capture.captureError, {limit:2});
     }
-};
+}
