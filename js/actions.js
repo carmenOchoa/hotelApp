47fbee1878415c3f17460db1143e3a353dc89fc8
carmenOchoa <carmen_ochoa@laitaliana.com.mx>
2016-04-16 12:39:06 -0500

    a

--- js/actions.js
+++ js/actions.js
@@ -3,10 +3,15 @@ var fn = {
         document.addEventListener("deviceready", fn.init, false);
     },
     init: function(){
+        //funcionalidades para el registro
         if(!fn.estaRegistrado())
             window.location.href = '#registro';
-        $('#registro div[data-role=footer] a').click(fn.regitrar);
-        $('#tomarFoto').click(capture.takePhoto);
+        $('#registro div[data-role=footer] a').tap(fn.regitrar);
+        $('#tomarFoto').tap(capture.takePhoto);
+        //funcionalidades para reservar
+        $('#nr1 div[data-role=navbar] a:eq(0)').tap(fn.siguientePaso);
+        $('#nr2 ul[data-role=listview] a').tap(fn.seleccionaHabitacion);
+        $('#nr2 div[data-role=navbar] a:eq(0)').tap(fn.obtenerReserva);
     },
     //***********funciones de registro**********************
     estaRegistrado: function(){
@@ -40,6 +45,34 @@ var fn = {
                 });
             }else
             alert('Todos los campos son requeridos');
+    },
+    //***********funciones de reserva**********************
+    per: '',
+    dia: '',
+    th: '',
+    siguientePaso: function(){
+        fn.per = $('#nrPer').val();
+        fn.dia = $('#nrDia').val();
+    
+        if(fn.per != '' && fn.dia != '')
+            window.location.href = "#nr2";
+        else
+            navigator.notification.alert("Todos los campos son requeridos", null, "Error al llenar", "Aceptar");
+    },
+    seleccionaHabitacion: function(){
+        $(this).parent().parent().find('a').css('background-color','transparent');
+        $(this).css('background-color','#00ff00');
+        fn.th = $(this).parent().index();
+    },
+    obtenerReserva: function(){
+        if (fn.th != ''){
+            if(navigator.connection.type != Connection.NONE)
+                //Enviar a servidor
+            else
+                //Guadar localmente
+        }
+        else
+            navigator.notification.alert("Debe seleccionar tipo de habitacion", null, "Error al llenar", "Aceptar");
     }
 };
 
